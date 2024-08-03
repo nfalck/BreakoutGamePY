e67a8e92c528f4db18c78b53b9c5f6caaadd263e
nfalck <nathaliefalck@protonmail.com>
2024-08-03 15:51:36 +0200

    Added collision with walls Added collision with paddle Added collision with bricks Added Ball class Added Brick and Bricks classes

--- main.py
+++ main.py
@@ -1,20 +1,132 @@
 import turtle
 import tkinter as tk
 from PIL import Image, ImageTk
+import random
+
+class Brick():
+    def __init__(self, screen, color):
+        self.brick = turtle.RawTurtle(screen)
+        self.brick.shape(color)
+        self.brick.penup()
+        self.brick_top = self.brick.ycor()+33
+        self.brick_right = self.brick.xcor()+30
+        self.brick_left = self.brick.xcor()-30
+        self.brick_bottom = self.brick.ycor()-33
+
+
+    def generate(self, x, y):
+        self.brick.goto(x,y)
+        # borders
+
+class Bricks:
+    def __init__(self, selection, screen, color):
+        self.bricks = []
+        self.brick_generation(selection=selection, screen=screen, color=color)
+
+
+    def brick_generation(self, selection, screen, color):
+        if selection == 1:
+            rows = 6
+            cols = 7
+            for row in range(rows):
+                for col in range(cols):
+                    x = -300 + col * 100
+                    y = 300 - row * 30
+                    brick = Brick(screen, color)
+                    self.bricks.append(brick)
+                    brick.brick.goto(x,y)
+
+
+
+
+class Ball:
+    def __init__(self, selection, screen):
+        if selection == 1:
+            self.ball_image = r"assets/ball_1.gif"
+            screen.register_shape(self.ball_image)
+            self.ball = turtle.RawTurtle(screen, shape=self.ball_image, visible=False)
+        elif selection == 2:
+            self.ball_image = r"assets/ball_2.gif"
+            screen.register_shape(self.ball_image)
+            self.ball = turtle.RawTurtle(screen, shape=self.ball_image, visible=False)
+        elif selection == 3:
+            self.ball_image = r"assets/ball_3.gif"
+            screen.register_shape(self.ball_image)
+            self.ball = turtle.RawTurtle(screen, shape=self.ball_image, visible=False)
+        self.ball.penup()
+        self.ball.goto(0,-355)
+        self.ball.showturtle()
+        self.ball_x_move = 10
+        self.ball_y_move = 10
+
+    def move(self):
+        new_x = self.ball.xcor() + self.ball_x_move
+        new_y = self.ball.ycor() + self.ball_y_move
+        self.ball.goto(x=new_x, y=new_y)
+
+    def collision_with_walls(self):
+        if self.ball.xcor() >= 375:
+            self.ball_x_move *= -1
+        elif self.ball.xcor() <= -375:
+            self.ball_x_move *= -1
+        elif self.ball.ycor() >= 375:
+            self.ball_y_move *= -1
+
+    def collision_with_paddle(self, paddle):
+        if (self.ball.distance(paddle.paddle) < 100 / 2 + 10 and
+                self.ball.ycor() > paddle.paddle.ycor() and
+                self.ball.ycor() < paddle.paddle.ycor() + 10):
+            self.ball_y_move *= -1
+
+    def collision_with_bricks(self, bricks):
+        for brick in bricks.bricks:
+            if self.ball.distance(brick.brick) < 40:
+                brick.brick.clear()
+                brick.brick.hideturtle()
+                bricks.bricks.remove(brick)
+
+                # detect collision from left
+            if self.ball.xcor() < brick.brick_left:
+                self.ball_x_move *= -1
+
+                # detect collision from right
+            elif self.ball.xcor() > brick.brick_right:
+                self.ball_x_move *= -1
+
+                # detect collision from bottom
+            elif self.ball.ycor() > brick.brick_bottom:
+                self.ball_y_move *= -1
+
+                # detect collision from top
+            elif self.ball.ycor() > brick.brick_top:
+                self.ball_y_move *= -1
+
+
 
 class Paddle:
     def __init__(self, selection, screen):
-        self.paddle_image = r"assets/paddle_1.gif"
-        screen.register_shape(self.paddle_image)
-        self.paddle = turtle.RawTurtle(screen, shape=self.paddle_image, visible=False)
+        if selection == 1:
+            self.paddle_image = r"assets/paddle_1.gif"
+            screen.register_shape(self.paddle_image)
+            self.paddle = turtle.RawTurtle(screen, shape=self.paddle_image, visible=False)
+        elif selection == 2:
+            self.paddle_image = r"assets/paddle_2.gif"
+            screen.register_shape(self.paddle_image)
+            self.paddle = turtle.RawTurtle(screen, shape=self.paddle_image, visible=False)
+        elif selection == 3:
+            self.paddle_image = r"assets/paddle_3.gif"
+            screen.register_shape(self.paddle_image)
+            self.paddle = turtle.RawTurtle(screen, shape=self.paddle_image, visible=False)
         self.paddle.penup()
-        self.paddle.goto(0,-380)
+        self.paddle.goto(0, -380)
         self.paddle.showturtle()
 
     def left(self):
-        self.paddle.bk(25)
+        if self.paddle.xcor() > -350:
+            self.paddle.bk(25)
     def right(self):
-        self.paddle.fd(25)
+        if self.paddle.xcor() < 350:
+            self.paddle.fd(25)
 
 
 class Game:
@@ -28,17 +140,33 @@ class Game:
             self.screen = turtle.TurtleScreen(self.canvas)
             self.screen.bgpic('assets/level1_background.png')
             self.screen.update()
-            self.game_paddle = Paddle(selection=self.selection, screen=self.screen)
-            self.game_start()
         if self.selection == 2:
-            pass
+            self.canvas.create_image(0, 0, image=level_backgrounds[1], anchor="nw")
+            self.screen = turtle.TurtleScreen(self.canvas)
+            self.screen.bgpic('assets/level2_background.png')
         elif self.selection == 3:
-            pass
+            self.canvas.create_image(0, 0, image=level_backgrounds[2], anchor="nw")
+            self.screen = turtle.TurtleScreen(self.canvas)
+            self.screen.bgpic('assets/level3_background.png')
+            self.screen.update()
+        self.game_paddle = Paddle(selection=self.selection, screen=self.screen)
+        self.game_ball = Ball(selection=self.selection, screen=self.screen)
+        self.brick_color = r"assets/brick_1.gif"
+        self.screen.register_shape(self.brick_color)
+        self.game_bricks = Bricks(selection=self.selection, screen=self.screen, color=self.brick_color)
+        self.screen.listen()
+        self.screen.onkey(self.game_start, "space")
+        global game_playing
+        game_playing = True
 
     def game_start(self):
-        self.screen.listen()
-        self.screen.onkey(self.game_paddle.left, "Left")
-        self.screen.onkey(self.game_paddle.right, "Right")
+        while game_playing:
+            self.screen.onkey(self.game_paddle.left, "Left")
+            self.screen.onkey(self.game_paddle.right, "Right")
+            self.game_ball.move()
+            self.game_ball.collision_with_walls()
+            self.game_ball.collision_with_paddle(self.game_paddle)
+            self.game_ball.collision_with_bricks(bricks=self.game_bricks)
 
 class MainApplication:
     def __init__(self, master):
@@ -47,7 +175,9 @@ class MainApplication:
         self.start_button = tk.Button(self.master, text="Start Game", command=self.select_level)
         self.start_button.grid(row=0, column=1)
         self.level1_background = tk.PhotoImage(file="assets/level1_background.png")
-        self.level_backgrounds = [self.level1_background]
+        self.level2_background = tk.PhotoImage(file="assets/level2_background.png")
+        self.level3_background = tk.PhotoImage(file="assets/level3_background.png")
+        self.level_backgrounds = [self.level1_background, self.level2_background, self.level3_background]
 
     def select_level(self):
         for widget in root.winfo_children():
