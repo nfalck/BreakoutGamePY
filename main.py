1fd68941a3efd4b322a86c62421965f7835d5274
nfalck <nathaliefalck@protonmail.com>
2024-08-07 19:45:51 +0200

    Redid GUI Replaced buttons with customtkinter for Mac support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import tkinter as tk
+import customtkinter as ctk
 from PIL import Image, ImageTk
 import time
 from ScoreBoard import ScoreBoard
@@ -13,8 +14,12 @@ class MainApplication:
         # Main Menu layout
         self.master = master
         self.master.title("Breakout Game")
-        self.start_button = tk.Button(self.master, text="Start Game", command=self.select_level)
-        self.start_button.grid(row=0, column=1)
+        self.startmenu_text = tk.Label(self.master, text="Breakout Game", font=('Lexend', 18, 'normal'), fg="white",
+                                       background="#AF47D2", pady=50, padx=50)
+        self.startmenu_text.grid(row=0, column=0)
+        self.start_button = ctk.CTkButton(self.master, text="Start Game", command=self.select_level, fg_color="#26355D",
+                                          corner_radius=10, border_width=2, bg_color="#AF47D2", border_color="white")
+        self.start_button.grid(row=1, column=0)
 
         # Background images for every level
         self.level1_background = tk.PhotoImage(file="assets/level1_background.png")
@@ -22,7 +27,7 @@ class MainApplication:
         self.level3_background = tk.PhotoImage(file="assets/level3_background.png")
 
         #  Level Selection Widgets for select_level
-        self.select_title = tk.Label(self.master, text="Select Level")  # Title
+        self.select_title = tk.Label(self.master, text="Select Level", fg="white", background="#AF47D2")  # Title
         self.level1_image = ImageTk.PhotoImage(Image.open("assets/level1_preview.png"))  # Preview of level 1
         self.level2_image = ImageTk.PhotoImage(Image.open("assets/level2_preview.png"))  # Preview of level 2
         self.level3_image = ImageTk.PhotoImage(Image.open("assets/level3_preview.png"))  # Preview of level 3
@@ -36,30 +41,33 @@ class MainApplication:
         self.level3_button = self.level3_button = tk.Radiobutton(self.master, variable=self.var, value=3,
                                                                  indicatoron=False, image=self.level3_image,
                                                                  command=self.update_selection)
-        self.selection_label = tk.Label(self.master, text="")  # Indicates which level is selected
-        self.start_game_button = tk.Button(self.master, text="Start Game", command=self.initialize_game)
+        self.selection_label = tk.Label(self.master, text="",
+                                        background="#AF47D2", fg="white")  # Indicates which level is selected
+        self.start_game_button = ctk.CTkButton(self.master, text="Start Game", command=self.initialize_game,
+                                               fg_color="#26355D", corner_radius=10, border_width=2,
+                                               bg_color="#AF47D2", border_color="white")
 
         self.selection = None  # initialized in update_selection
 
         # Game Widgets for start_game
         self.canvas = tk.Canvas(self.master, width=800, height=800)
-        self.start_frame = tk.Frame(self.canvas, background="blue", width=200, height=200)
-        self.start_text = tk.Label(self.canvas, text="Press space to start", fg="white", bg="blue")
+        self.start_frame = tk.Frame(self.canvas, background="#AF47D2", width=200, height=200)
+        self.start_text = tk.Label(self.canvas, text="Press space to start", fg="white", bg="#AF47D2")
 
         # Game Assets that will be initialized in start_game
-        self.screen = None # TurtleScreen
+        self.screen = None  # TurtleScreen
         self.scoreboard = None
         self.game_paddle = None
         self.game_ball = None
         self.game_bricks = None
 
         # Game Win Widgets for game_win
-        self.win_frame = tk.Frame(self.canvas, background="blue", width=200, height=200)
-        self.win_label = tk.Label(self.canvas, text="You Won!", fg="white", bg="blue")
+        self.win_frame = tk.Frame(self.canvas, background="#AF47D2", width=200, height=200)
+        self.win_label = tk.Label(self.canvas, text="You Won!", fg="white", bg="#AF47D2")
 
         # Gameover Widgets for game_over
-        self.gameover_frame = tk.Frame(self.canvas, background="blue", width=200, height=200)
-        self.gameover_label = tk.Label(self.canvas, text="Game Over", fg="white", bg="blue")
+        self.gameover_frame = tk.Frame(self.canvas, background="#AF47D2", width=200, height=200)
+        self.gameover_label = tk.Label(self.canvas, text="Game Over", fg="white", bg="#AF47D2")
 
     def select_level(self):
         """ Generate the menu of level selection """
@@ -125,8 +133,8 @@ class MainApplication:
         self.game_bricks = Bricks(selection=self.selection, screen=self.screen)
 
         # Start "popup" to instruct the player to press space in order to start playing
-        self.start_frame.place(x=300, y=400)
-        self.start_text.place(x=300, y=450)
+        self.start_frame.place(x=300, y=300)
+        self.start_text.place(x=315, y=380)
 
         # Screen starts listening for any key presses
         self.screen.listen()
@@ -142,22 +150,26 @@ class MainApplication:
         quit the application """
         # Place the widgets needed
         self.win_frame.place(x=300, y=400)
-        self.win_label.place(x=350, y=450)
-        self.menu_button = tk.Button(self.canvas, text="Main Menu", command=self.select_level)
-        self.menu_button.place(x=325, y=500)
-        self.quit_button = tk.Button(self.canvas, text="Quit", command=self.master.destroy)
-        self.quit_button.place(x=425, y=500)
+        self.win_label.place(x=355, y=350)
+        self.menu_button = ctk.CTkButton(self.canvas, text="Main Menu", command=self.select_level, width=60,
+                                         fg_color="#26355D", corner_radius=10, bg_color="#AF47D2")
+        self.menu_button.place(x=315, y=400)
+        self.quit_button = ctk.CTkButton(self.canvas, text="Quit", command=self.master.destroy, width=30,
+                                         fg_color="#26355D", corner_radius=10, bg_color="#AF47D2")
+        self.quit_button.place(x=435, y=400)
 
     def game_over(self):
         """ Generate the gameover "popup" to indicate player's loss and give option to go to main menu or
         quit the application """
         # Place the widgets needed
-        self.gameover_frame.place(x=300, y=400)
-        self.gameover_label.place(x=350, y=450)
-        self.menu_button = tk.Button(self.canvas, text="Main Menu", command=self.select_level)
-        self.menu_button.place(x=325, y=500)
-        self.quit_button = tk.Button(self.canvas, text="Quit", command=self.master.destroy)
-        self.quit_button.place(x=425, y=500)
+        self.gameover_frame.place(x=300, y=300)
+        self.gameover_label.place(x=355, y=350)
+        self.menu_button = ctk.CTkButton(self.canvas, text="Main Menu", command=self.select_level, width=60,
+                                         fg_color="#26355D", corner_radius=10, bg_color="#AF47D2")
+        self.menu_button.place(x=315, y=400)
+        self.quit_button = ctk.CTkButton(self.canvas, text="Quit", command=self.master.destroy, width=30,
+                                         fg_color="#26355D", corner_radius=10, bg_color="#AF47D2")
+        self.quit_button.place(x=435, y=400)
 
     def game_start(self):
         """ Start the game and its logic when player presses space """
@@ -194,7 +206,7 @@ class MainApplication:
 if __name__ == "__main__":
     # Create the tkinter window
     root = tk.Tk()
-    root.config(width=800, height=800)
+    root.config(width=800, height=800, background="#AF47D2")
     # Create an instance of the MainApplication class and start the main event loop
     mainapplication = MainApplication(root)
     root.mainloop()
